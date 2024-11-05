de8a57272a7cbbe19818a09d0a585b75e5219a9f
Yewon Cho <qjqmti0113@gmail.com>
2024-11-05 15:28:13 +0900

    1105 cho - WebConfg.java 수정

--- src/main/java/com/choongang/frombirth_backend/config/WebConfig.java
+++ src/main/java/com/choongang/frombirth_backend/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Value("${frontEnd.url}")
-    private String frontEndUrl;
+    private String frontEndUrl; //
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
